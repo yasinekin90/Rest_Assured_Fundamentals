162b3cb1a708b4b73c32fb8b5b5116e4a0ac1bf9
Yasin <ekinyasin90@gmail.com>
2022-12-23 19:51:12 +0300

    some api queries added

--- src/test/java/jira/JiraTest.java
+++ src/test/java/jira/JiraTest.java
@@ -3,6 +3,7 @@ package jira;
 import io.restassured.RestAssured;
 import io.restassured.filter.session.SessionFilter;
 import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 
 import java.io.File;
 
@@ -14,7 +15,9 @@ public class JiraTest {
         baseURI = "http://localhost:8081";
         //Login Scenario
         //get session id
-        SessionFilter sessionFilter=new SessionFilter(); //you can store response in sessionFilter variable
+        SessionFilter sessionFilter = new SessionFilter(); //you can store response in sessionFilter variable
+
+
         String response = given().header("Content-Type", "application/json")
                 .body("{\n" +
                         "    \"username\":\"ekinyasin90\",\n" +
@@ -23,13 +26,12 @@ public class JiraTest {
                 .then().log().all().extract().response().asString();//produces a session id
 
 
-
-
-
-        given().pathParam("id", "10100").log().all()
+        String expectedMessage = "Hi How are you?";
+        //Add comment
+        String addCommentResponse = given().pathParam("id", "10100").log().all()
                 .header("Content-Type", "application/json")
                 .body("{\n" +
-                        "    \"body\": \"This is my first comment\",\n" +
+                        "    \"body\": \"" + expectedMessage + "\",\n" +
                         "    \"visibility\": {\n" +
                         "        \"type\": \"role\",\n" +
                         "        \"value\": \"Administrators\"\n" +
@@ -37,17 +39,42 @@ public class JiraTest {
                         "}")
                 .filter(sessionFilter) //get the session id from session filter
                 .when().post("/rest/api/2/issue/{id}/comment")
-                .then().log().all().assertThat().statusCode(201);
+                .then().log().all().assertThat().statusCode(201).extract().response().asString();
+        JsonPath js = new JsonPath(addCommentResponse);
+        String idComment = js.getString("id");
 
 
         //Add attachment
-        given().header("X-Atlassian-Token","no-check")
+       /* given().header("X-Atlassian-Token","no-check")
                 .filter(sessionFilter)
                 .pathParam("id", "10100")
                 .header("Content-Type","multipart/form-data")
                 .multiPart("file",new File("jira.txt")) //adding attachment
                 .when().post("/rest/api/2/issue/{id}/attachments")
-                .then().log().all().assertThat().statusCode(200);
+                .then().log().all().assertThat().statusCode(200);*/
 
+        //Get Issue
+
+        String issueDetails = given().filter(sessionFilter)
+                .pathParam("Key", "10100")
+                .queryParam("fields", "comment")
+                .when().get("/rest/api/2/issue/{Key}")
+                .then().log().all().extract().response().asString();
+
+        System.out.println("issueDetails = " + issueDetails);
+
+        JsonPath jsonPath = new JsonPath(issueDetails);
+
+        int commentsCount = jsonPath.getInt("fields.comment.comments.size()");
+        String message=null;
+        for (int i = 0; i < commentsCount; i++) {
+
+            String commentIdIssue = jsonPath.get("fields.comment.comments[" + i + "].id").toString();
+            if (commentIdIssue.equalsIgnoreCase(idComment)) {
+                 message = jsonPath.get("fields.comment.comments[" + i + "].body").toString();
+            }
+        }
+
+        Assert.assertEquals(message,expectedMessage);
     }
 }
